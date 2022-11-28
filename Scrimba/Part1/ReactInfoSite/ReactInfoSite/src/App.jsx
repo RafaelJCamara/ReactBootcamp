60f01573bcb6685e4dc5ffebe0cc3de4b7cab004
RafaelJCamara <rafaelcamarac@gmail.com>
2022-11-28 22:30:10 +0000

    Add first react section project.

---
+++ Scrimba/Part1/ReactInfoSite/ReactInfoSite/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import MainContent from './MainContent'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+    </div>
+  )
+}
+
+export default App
---
+++ Scrimba/Part1/ReactInfoSite/ReactInfoSite/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './Navbar.css'
+
+
+function Navbar(){
+    return (
+        <nav className='navbar'>
+            <img src="https://www.datocms-assets.com/45470/1631110818-logo-react-js.png" width="200px" />
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default Navbar;
